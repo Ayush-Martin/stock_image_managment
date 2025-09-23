291ee7f65f0bae733cc24ef11da231ca91dcf593
Ayush Martin Sabu <ayushmartin06@gmail.com>
2025-09-23 16:51:56 +0530

    initilized client and CI CD

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="bg-app-bg">
+      App
+      <Button>Hello</Button>
+    </div>
+  );
+};
+
+export default App;
